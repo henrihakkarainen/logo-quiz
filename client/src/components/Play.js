8855a14524f6170e3c1279b25fbda09ee8e319ad
Henri Hakkarainen <henri.hakkarainen@tuni.fi>
2021-02-16 18:23:29 +0200

    More stuff to play-view

--- client/src/components/Play.js
+++ client/src/components/Play.js
@@ -3,13 +3,11 @@ import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 
+import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
 
 import '../styles/Game.css';
-
-const useQuery = () => {
-  return new URLSearchParams(useLocation().search);
-}
 
 const Loading = () => {
   return (
@@ -17,21 +15,74 @@ const Loading = () => {
   );
 }
 
+const StartScreen = (props) => {
+  const { t } = props;
+
+  return (
+    <div className="start-screen">
+      <Typography variant="h4">
+        {props.title}
+      </Typography>
+      <Typography>
+        {props.description}
+      </Typography>
+      <Typography>
+        {t('game.description')}
+      </Typography>
+      <Typography>
+        {t('game.goodLuck')}
+      </Typography>
+      <Button variant="contained" color="primary">
+        {t('game.start')}
+      </Button>
+    </div>
+  );
+}
+
+const Error = () => {
+  return (
+    <div>
+      Game not found
+    </div>
+  )
+}
+
 const Play = (props) => {
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  }
+
+  const [ t, i18n ] = useTranslation();
   const [ category, setCategory ] = useState(useQuery().get('category'))
-  const [ game, setGame ] = useState([]);
+  const [ game, setGame ] = useState(null);
   const [ loading, setLoading ] = useState(true);
-  console.log(game)
 
   useEffect(() => {
     axios.get(`http://localhost:8080/api/categories?title=${category}`)
-      .then(res => setGame(res.data))
-      .then(setLoading(false))
+      .then(res => {
+        setGame(res.data[0])
+        setLoading(false)
+      })
   }, [category])
 
+  const renderGame = () => {
+
+  }
+  
+  console.log(game)
   return (
     <div className="game">
-      <Loading />
+      {
+        loading ?
+        <Loading /> :
+        game ?
+        <StartScreen
+          title={game.title[i18n.language]}
+          description={game.description[i18n.language]}
+          t={t} /> :
+        <Error />
+      }
     </div>
   );
 }
